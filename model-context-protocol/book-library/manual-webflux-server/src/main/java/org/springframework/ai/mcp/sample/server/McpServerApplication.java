3598e474d786336650ba03860b9c8bb39c9bf91e
lvasco <108840419+lvasco@users.noreply.github.com>
2025-03-20 09:22:15 -0300

    [GFTCodeFixer]: Update on model-context-protocol/book-library/manual-webflux-server/src/main/java/org/springframework/ai/mcp/sample/server/McpServerApplication.java

--- model-context-protocol/book-library/manual-webflux-server/src/main/java/org/springframework/ai/mcp/sample/server/McpServerApplication.java
+++ model-context-protocol/book-library/manual-webflux-server/src/main/java/org/springframework/ai/mcp/sample/server/McpServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class McpServerApplication {
+    private McpServerApplication() { }
 
 	public static void main(String[] args) {
 		SpringApplication.run(McpServerApplication.class, args);
